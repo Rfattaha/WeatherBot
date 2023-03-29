ef412cd3900f67b07196fddef09acdb2f391d8a0
Rfattaha <tahacimen.05@outlook.com>
2023-03-29 16:31:50 +0200

    Finish

---
+++ tlbot.py
@@ -0,0 +1,47 @@
+from telebot import types
+import telebot
+from weather import *
+
+bot = telebot.TeleBot("6201525049:AAFdl6-H4Hp8XsTx0gyodiqBibfskTtFsjc", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+
+
+
+kb = types.InlineKeyboardMarkup(row_width=2)
+
+words = [
+'London' ,
+'Istanbul',
+'Moscow' ,
+'Berlin',
+'Type' 
+]
+
+
+for item in words:
+    kb.add(types.InlineKeyboardButton(item, callback_data = item))
+
+
+
+@bot.message_handler(commands=['start', 's'])
+def post_random_article(message):
+    word = 'Select the city'
+    bot.send_message(message.from_user.id, word, reply_markup = kb)
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+  message_text = message.text 
+  cid = message.chat.id
+  bot.send_message(text=getResp(message_text),chat_id=cid)
+  
+
+
+@bot.callback_query_handler(func=lambda call:True )
+def handle_query(call):
+   if call.data == "Type":
+       bot.send_message(call.from_user.id, 'Please Enter The City : ')
+   else:
+        bot.send_message(call.from_user.id,getResp(call.data))
+
+
+
+bot.infinity_polling()
---
+++ weather.py
@@ -0,0 +1,35 @@
+import requests
+
+def getResp(city):
+    response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=e61e09d2a1525b6751944db317373c0d&units=metric" % (city))
+    getCountry = response.json()["sys"]["country"]
+    getCityName = response.json()["name"]
+    getWeatherMain = response.json()["weather"][0]["description"]
+    getTemp = response.json()["main"]["temp"]
+
+    a = ("Country : {} , \nCity name : {} , \nCity Weather Description : {} , \nCity Calcius : {} °C ".format(getCountry,getCityName,getWeatherMain,getTemp) )
+          
+    return a
+    #print("City name : ",response.json()["name"])
+    #print("City Weather Description : ", response.json()["weather"][0]["description"])
+    #print("City Calcius is : ",response.json()["main"]["temp"])
+    #print("Country : ",response.json()["sys"]["country"] ,"\nCity name : "
+    #      ,response.json()["name"],"\nCity Weather Description : "
+    #      , response.json()["weather"][0]["description"]
+    #      ,"\nCity Calcius is : ",response.json()["main"]["temp"])
+
+resp = getResp("istanbul")
+
+def getTemp():
+    return resp.json()["main"]["temp"]
+
+def getWeatherMain():
+    return resp.json()["weather"][0]["description"]
+
+def getCountry():
+    return resp.json()["sys"]["country"] 
+
+def getCityName():
+    return resp.json()["name"]
+
+
